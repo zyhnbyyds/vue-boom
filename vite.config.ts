f1fa14c662feb2ab16e57eca5116efce666aaf0a
YuHangZhang <19939926438@163.com>
2025-06-08 14:52:51 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import path from 'node:path'
+import Vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        '@vueuse/core',
+      ],
+      dts: true,
+      dirs: [
+        './src/composables',
+      ],
+      vueTemplate: true,
+    }),
+    Components({
+      dts: true,
+    }),
+    UnoCSS(),
+    Vue(),
+  ],
+
+  // https://github.com/vitest-dev/vitest
+  test: {
+    environment: 'jsdom',
+  },
+})
