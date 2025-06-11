35dd2d2b4765dc3783fb1b6cdb301f2c64d5efe4
zyhnbyyds <19939926438@163.com>
2025-06-11 20:00:53 +0800

    refactor: remove unused files and components

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import type { RouteRecordRaw } from 'vue-router'
-
-export const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    component: () => import('../pages/index.vue'),
-    name: 'home',
-  },
-]
